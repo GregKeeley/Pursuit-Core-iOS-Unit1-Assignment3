30034cae698e61ffb3b7d4c0ef04881eff1ef5da
Gregory Keeley <gregkeeley@Gregorys-MacBook-Pro.local>
2019-10-17 17:54:30 -0400

    Updated project

--- Calculator/Calculator/main.swift
+++ Calculator/Calculator/main.swift
@@ -24,23 +24,9 @@ func mathStuffFactory(opString: String) -> (Double, Double) -> Double {
 }
 
 let additionOpertation = mathStuffFactory(opString: "+")
-
 let multiplecationOp = mathStuffFactory(opString: "*")
-
 let divisionOp = mathStuffFactory(opString: "/")
-
 let subtractionOp = mathStuffFactory(opString: "-")
-
-
-let result = additionOpertation(45, 5)
-let resultMutliple = multiplecationOp(5, 5)
-let resultDivision = divisionOp(100, 10)
-let resultSubtraction = subtractionOp(500, 400)
-
-print("Result of addition operation is \(result)")
-print("Result of subtraction operation is \(resultSubtraction)")
-print("Result of division operation is \(resultDivision)")
-print("Result of multplication operation is \(resultMutliple)")
 
 var repeatCondition = true // Repeat condition
 var userArray = [String]()
@@ -53,21 +39,22 @@ repeat {
     
     let userInput = readLine() ?? "1 + 1" // Taking user input
     userArray = userInput.components(separatedBy: " ")
+    
     if userArray.count == 3 && userArray[1] == "?" {
         print("This used a random operator")
         userArray[1] = randomOperation.randomElement() ?? "+"
         randomOpUsed = userArray[1]
         randomOpCheck = true
     }
-        if userArray.count == 3 {
-            let op1 = Double(userArray[0]) ?? 0.0
-            let op2 = Double(userArray[2]) ?? 0.0
-            let closureOp = mathStuffFactory(opString: userArray[1])
-            let userResult = closureOp(op1,op2)
-            print(userResult)
-        } else {
-            print("Try again")
-        }
+    if userArray.count == 3 {
+        let op1 = Double(userArray[0]) ?? 0.0
+        let op2 = Double(userArray[2]) ?? 0.0
+        let closureOp = mathStuffFactory(opString: userArray[1])
+        let userResult = closureOp(op1,op2)
+        print(userResult)
+    } else {
+        print("Try again")
+    }
     if randomOpCheck == true {
         print("Can you guess random operator just used? Choices: *, +, -, /")
         let userGuess = readLine()
@@ -77,12 +64,24 @@ repeat {
             print("Nope.")
         }
     }
-        print("Would you like to continue calculating?")
-        let contPrompt = readLine()?.lowercased() ?? "no"
-        if contPrompt == "yes" {
-            continue
-        } else {
-            repeatCondition = false
-        }
+    print()
+    print("Would you like to continue calculating?")
+    let contPrompt = readLine()?.lowercased() ?? "no"
+    if contPrompt == "yes" {
+        continue
+        
+    } else {
+        repeatCondition = false
     }
+}
     while repeatCondition
+
+//func myFilter(inputArray: [Int], filter: (Int) -> Bool) -> [Int] {
+//    let result = [Int]()
+//    var filterNum = filter
+//    for num in inputArray {
+//        if num < filterNum {
+//    }
+//    return result
+//}
+//}
